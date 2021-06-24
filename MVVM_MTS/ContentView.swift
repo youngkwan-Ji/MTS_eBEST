a55ae31df3e04c36079e2b66a6b0c2e1d86e8f6e
youngkwan-Ji <youngkwanji@gmail.com>
2021-06-24 09:46:46 +0900

    project init

---
+++ MVVM_MTS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM_MTS
+//
+//  Created by youngkwanji on 2021/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVM_MTS/MVVM_MTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_MTSApp.swift
+//  MVVM_MTS
+//
+//  Created by youngkwanji on 2021/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_MTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
